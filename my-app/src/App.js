d46d7246c927012fc7c7b135d5b1c2083da1b7e4
VladimirDev685 <vlad.frontdev@gmail.com>
2024-08-05 04:17:05 +0300

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import moment from 'moment';
+import { createElement } from 'react';
 
 const currentDate = moment().format('YYYY');//декларативный подход
-
-export const App = () => {//декларативный подход
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{currentDate}
-			</header>
-		</div>
+const form = function Form() { //декларативный подход
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement('p',null,'Edit ',createElement('code',null,'src/App.js'), ' and save to reload'),
+			createElement('a',{className:'App-link',
+				href:'https://reactjs.org',
+				target:'_blank',
+				rel:'noopener noreferrer'},'Learn React'),
+				currentDate
+		),
 	);
+};
+export const App = () => {//декларативный подход
+	return createElement(form);//декларативный подход
 };
